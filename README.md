e07feefe6fc8392bfc3b6e72061f9fd767236249
Margus Pala <margus.pala@gmail.com>
2016-09-04 19:58:28 +0300

    Sample ready

---
+++ README.md
@@ -0,0 +1,24 @@
+# README #
+
+This sample application is one of the easiest ways to get up and running with Smart-ID secure login service.
+
+### How do I get set up? ###
+
+For beginners only, sorry coders!
+
+* Open terminal, login to your server 
+* Upload the files from repository  with command
+```
+#!bash
+
+git clone https://bitbucket.org/smartid/nodesample
+```
+
+* Go to https://api.smartid.ee and register your website with catalog https://mydomen.ee/. Oauth redirect_uri can be the same.
+* Copy the Public and Secret keys and  redirect uri to the mydomen.ee/main.js 
+
+* Start the nodejs process and open the application at mydomen.ee/ and click on eIDAS logo to start the login process!
+
+### Who do I talk to? ###
+
+* With all questions please turn to help@smartid.ee
---
+++ main.js
@@ -0,0 +1,79 @@
+//!!!!!!!!!!!!!!! Following variables need changing !!!!!!!!!!!!!!!!!!!!!!
+var clientId = 'here_put_your_public_key';
+var clientSecret = 'here_put_your_secret_key';
+var redirect_uri = "http://mydomen.ee/samplescript";
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+var express = require('express'),
+        app = express();
+var request = require('request');
+
+var oauth2 = require('simple-oauth2')({
+    clientID: clientId,
+    clientSecret: clientSecret,
+    site: 'https://api.smartid.ee',
+    tokenPath: '/oauth/access_token',
+    authorizationPath: '/oauth/authorize'
+});
+
+// Authorization uri definition
+var authorization_uri = oauth2.authCode.authorizeURL({
+    redirect_uri: redirect_uri
+});
+
+
+//Make it possible to show image
+app.use(express.static(__dirname));
+
+
+// Initial page redirecting to Github
+app.get('/auth', function (req, res) {
+
+});
+
+// Callback service parsing the authorization token and asking for the access token
+app.get('/', function (req, res) {
+    var code = req.query.code;
+    var login = req.query.login;
+    console.log(code + ", " + login);
+    if (typeof code === 'undefined' && typeof login !== 'undefined') {
+        res.redirect(authorization_uri);
+        return;
+    } else if (typeof code !== 'undefined') {
+        oauth2.authCode.getToken({
+            code: code,
+            redirect_uri: redirect_uri
+        }, saveToken);
+    } else {
+        var url = req.protocol + '://' + req.get('host')
+        var pageHtml = '<strong>Click the image below to start login</strong><br>'
+                + '<a href="?login=true"><img src="'+url+'/eidas.jpg"></img></a>';
+        res.send(pageHtml);
+    }
+
+
+    function saveToken(error, result) {
+        if (error) {
+            console.log('Access Token Error', error.message);
+        }
+        console.log("Saving token");
+        token = oauth2.accessToken.create(result);
+        request({
+            url: 'https://api.smartid.ee/api/v2/user_data',
+            headers: {
+                "Authorization": "Bearer " + token.token.access_token
+            }
+
+        }, function (err, userResult) {
+            console.log("Got result");
+            console.log(userResult.body);
+            res.send(userResult.body);
+        });
+    }
+
+});
+
+var port = 3000;
+app.listen(port);
+
+console.log('Express server started on port ' + port);
